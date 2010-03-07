ed5a98e349e2a6ee4dab4ec7bfdbbcbc9bbbaacd
Justin Woods <justin.a.woods@gmail.com>
2010-03-07 22:13:59 +0000

    Initial commit of Video/Encode/Dvd.php

---
+++ Video/Encode/Dvd.php
@@ -0,0 +1,19 @@
+<?php
+
+class JW_Video_Encode_Dvd extends JW_Video_Encode_Abstract
+{
+
+    protected $_settings = 
+        '-aspect 4:3 -target ntsc-dvd';
+    
+    protected $_width		= 640;
+    protected $_height		= 480;
+    protected $_video_bitrate	= 2048;
+    protected $_audio_bitrate	= 128;
+
+    public function getCommand()
+    {
+        return $this->_getCommand();
+    }
+
+}
